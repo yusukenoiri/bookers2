3e4f866bb4e7eb1dfb38a5b32bd0fe274893615e
yusukenoiri <yusuke.noiri@gmail.com>
2021-05-03 12:09:27 +0000

    Update Ajax function

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,14 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
+    # @user = current_user
+    @user = User.find_by(id: @book.user_id)
+    
+    # @book_comment = BookComment.new
+    # redirect_to book_path(book)
   end
 
   def destroy
